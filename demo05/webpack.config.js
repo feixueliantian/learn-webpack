85344732cb9e9d193b6abce4dc0e7095bed64ede
任鹏 <ren2005peng@126.com>
2018-10-21 13:58:30 +0800

    添加：demo05 处理css

---
+++ demo05/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'none',
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
