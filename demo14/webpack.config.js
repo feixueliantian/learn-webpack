3456adcdf5c7558b7b4bea7e1026a1672335bb86
任鹏 <ren2005peng@126.com>
2018-10-22 10:28:50 +0800

    添加：demo14 Watch Mode && Clean Plugin

---
+++ demo14/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'none',
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
